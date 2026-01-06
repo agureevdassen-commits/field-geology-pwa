393803553495949632146e747c0150b27c2539b3
agureevdassen-commits <agureevdassen@gmail.com>
2026-01-06 10:32:43 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './styles/global.css'
 
 // Регистрация Service Worker
@@ -31,3 +31,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
